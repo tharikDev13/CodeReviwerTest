98612fa2b15a5aad30cad7f834fae32db9863cf2
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-07-31 12:16:21 +0530

    Initial commit

---
+++ CodeReviwerTest/CodeReviwerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReviwerTestApp.swift
+//  CodeReviwerTest
+//
+//  Created by Tharik anver  on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeReviwerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
