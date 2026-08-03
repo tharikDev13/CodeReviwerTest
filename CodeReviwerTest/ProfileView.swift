d533531dfb33be7c6ec7fbdc232def2151196b2b
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-08-03 15:07:20 +0530

    Added profile view.

---
+++ CodeReviwerTest/ProfileView.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileView.swift
+//  CodeReviwerTest
+//
+//  Created by Tharik anver  on 03/08/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+
+    @State var count = 0
+
+    var body: some View {
+
+        VStack {
+
+            ForEach(0..<1000) { i in
+
+                VStack {
+
+                    Text("User \(i)")
+                        .font(.headline)
+
+                    Text("Description")
+
+                    Button("Tap") {
+                        count += 1
+                        print(count)
+                    }
+                }
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+        }
+    }
+}
