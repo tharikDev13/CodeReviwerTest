fb395c3018126d65f6873a9e6c0308f8e084848f
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-08-03 15:56:26 +0530

    usermanagertwo added

---
+++ CodeReviwerTest/Usermanagertwo.swift
@@ -0,0 +1,41 @@
+//
+//  Usermanagertwo.swift
+//  CodeReviwerTest
+//
+//  Created by Tharik anver  on 03/08/26.
+//
+
+import Foundation
+
+class UserManager2 {
+
+    static let shared = UserManager()
+
+    var users: [String] = []
+
+    func loadUsers() {
+
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+
+            if error != nil {
+                print(error!)
+            }
+
+            let json = try! JSONSerialization.jsonObject(with: data!) as! [[String: Any]]
+
+            for item in json {
+                self.users.append(item["name"] as! String)
+            }
+
+            print(self.users)
+        }
+
+        task.resume()
+    }
+
+    func getUser(index: Int) -> String {
+        return users[index]
+    }
+}
